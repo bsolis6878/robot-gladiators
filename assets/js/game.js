62e440e64233c007f1ae8567aafd97c85b5f0c6b
bsolis6878 <bsolis6878@gmail.com>
2022-07-28 19:34:34 -0500

    fixed issue with fight not progressing to next robot

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,6 +4,9 @@
 //      * Defeath each enemy-robot
 // "LOSE" Player robot's health is zero or less
 
+// Alert players that they are starting the round
+window.alert("Welcome to Robot Gladiators!");
+
 var playerName = window.prompt("What is your robot's name?");
 
 var playerHealth = 100;
@@ -16,8 +19,8 @@ var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -70,8 +73,12 @@ var fight = function(enemyName) {
 } else {
     window.alert("You need to choose a valid option. Try again!");
 }
+    }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    // call fight function with enemy-robot
     fight(enemyNames[i]);
 }
