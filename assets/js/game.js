e608848df6ba9283e292534ad45340151eed6c92
bsolis6878 <bsolis6878@gmail.com>
2022-07-29 19:27:01 -0500

    added endgame function and replayability feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,11 +27,11 @@ var fight = function(enemyName) {
     // if player picks "skip" confirm and then stop the loop
     if (promptFight === "skip" || promptFight === "SKIP") {
         // confirm player wants to skip
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+        var confirmSkip = window.confirm("Are you sure you'd like to skip this fight?");
     
         // if yes (true), leave fight
         if (confirmSkip) {
-            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            window.alert(playerName + " has decided to skip this fight. On to the next round!");
             // subtract money from playerMoney for skipping
             playerMoney = playerMoney - 10;
             console.log("playerMoney", playerMoney);
@@ -76,15 +76,41 @@ var fight = function(enemyName) {
 } // end of while loop
     }; // end of fight function
 
+// function to end the entire game
+
+var endGame = function() {
+    // if  player is still alive, player wins!
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+    // ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        // restart the game
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+}
+
+// function to start a new game
+var startGame = function() {
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
 for(var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert("Robot Gladiators round " + ( i + 1 ) + ", start!")
-    }
-    else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
-    }
+
+
     // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
 
@@ -93,4 +119,17 @@ for(var i = 0; i < enemyNames.length; i++) {
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(enemyNames[i]);
+
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
+
+    // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    endGame();
+}
+
+// start the game when the page loads
+startGame();
